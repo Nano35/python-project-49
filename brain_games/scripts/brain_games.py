bede31896402f7357d711d09746336b03917034c
Nano35 <50954736+Nano35@users.noreply.github.com>
2022-09-15 18:23:32 +0300

    Add files via upload

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,1 +1,9 @@
+from brain_games.cli import run
 
+
+def main():
+    run()
+
+
+if __name__ == "__main__":
+    main()
---
+++ brain_games/scripts/brain_gcd.py
@@ -0,0 +1,10 @@
+from brain_games.game_engine import game_run
+from brain_games.games import gcd
+
+
+def main():
+    game_run(gcd)
+
+
+if __name__ == "__main__":
+    main()
